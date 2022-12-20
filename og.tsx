c7e54a0585ad00abe7b76f2cc32a300ff49b1fef
kota <52403688+yashikota@users.noreply.github.com>
2022-12-20 09:18:34 +0900

    fmt

--- og.tsx
+++ og.tsx
@@ -3,10 +3,11 @@ import React from "https://esm.sh/react@18.2.0";
 import { ImageResponse } from "https://deno.land/x/og_edge@0.0.4/mod.ts";
 const PORT = 8080;
 
-const fontURL = "http://themes.googleusercontent.com/static/fonts/notosans/v1/LeFlHvsZjXu2c3ZRgBq9nKCWcynf_cDxXwCLxiixG1c.ttf";
+const fontURL =
+  "http://themes.googleusercontent.com/static/fonts/notosans/v1/LeFlHvsZjXu2c3ZRgBq9nKCWcynf_cDxXwCLxiixG1c.ttf";
 const font = await fetch(fontURL).then((res) => res.arrayBuffer());
 
-const imageUrl = "https://raw.githubusercontent.com/yashikota/og/master/bg.png"
+const imageUrl = "https://raw.githubusercontent.com/yashikota/og/master/bg.png";
 
 const handler = async (req: Request) => {
   const fontData = await font;
